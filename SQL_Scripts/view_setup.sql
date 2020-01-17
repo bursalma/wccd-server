f5bda3184cdca011ff759cfd43c0c4bb0b728f0a
bursalma <bursalma@gmail.com>
2020-01-16 21:40:03 -0500

    insert statements

---
+++ SQL_Scripts/view_setup.sql
@@ -0,0 +1,21 @@
+use `white_collar`;
+
+show databases;
+
+select * from `convict`;
+select * from `conviction`;
+select * from `crime`;
+select * from `crime_conviction`;
+select * from `location`;
+select * from `location_conviction`;
+select * from `nationality`;
+select * from `occupation`;
+select * from `occupation_conviction`;
+select * from `race`;
+select * from `scheme`;
+select * from `scheme_conviction`;
+select * from `sector`;
+select * from `sector_conviction`;
+select * from `source_type`;
+select * from `source_type_conviction`;
+select * from `user`;
