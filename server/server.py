739b4ce82691bb1e06dded22344e10983f31cda1
bursalma <bursalma@gmail.com>
2020-02-11 21:34:13 -0500

    server code

--- server/server.py
+++ server/server.py
@@ -1,6 +1,108 @@
-from flask import Flask
+from flask import   Flask, request, url_for, abort, \
+                    session, redirect, flash
+from werkzeug.utils import secure_filename
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         f = request.files['the_file']
+#         f.save('/var/www/uploads/' + secure_filename(f.filename))
+
+# @app.route('/')
+# def index():
+#     return redirect(url_for('login'))
+
+# @app.route('/login')
+# def login():
+#     abort(401)
+#     this_is_never_executed()
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('page_not_found.html'), 404
+
+
+# return jsonify({})
+
+# @app.route("/me")
+# def me_api():
+#     user = get_current_user()
+#     return {
+#         "username": user.username,
+#         "theme": user.theme,
+#         "image": url_for("user_image", filename=user.image),
+#     }
+
+# @app.route("/users")
+# def users_api():
+#     users = get_all_users()
+#     return jsonify([user.to_json() for user in users])
+
+# # _______________ session ______________
+# # Set the secret key to some random bytes. Keep this really secret!
+# app.secret_key = b'N\xf2}#\xf0\xbb\x8f5MH\xde\xa6-\x04@k'
+
+# @app.route('/')
+# def index():
+#     if 'username' in session:
+#         return 'Logged in as %s' % escape(session['username'])
+#     return 'You are not logged in'
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         session['username'] = request.form['username']
+#         return redirect(url_for('index'))
+#     return '''
+#         <form method="post">
+#             <p><input type=text name=username>
+#             <p><input type=submit value=Login>
+#         </form>
+#     '''
+
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
+#     # _______________ session ______________
+
+# app.logger.debug('A value for debugging')
+# app.logger.warning('A warning occurred (%d apples)', 42)
+# app.logger.error('An error occurred')
+
+# pip install flask-sqlalchemy
+
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1',port=8000,debug=True)
+
+
+
+# from socket import gethostname
+# [...]
+
+# if __name__ == '__main__':
+#     db.create_all()
+#     if 'liveconsole' not in gethostname():
+#         app.run()
