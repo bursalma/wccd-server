ecce83393f64d8e48f28cdd306fa10e84ea69374
bursalma <bursalma@gmail.com>
2020-03-05 17:10:22 -0500

    xps initial

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,8 @@
 # start.sh
 
-pipenv shell
-pipenv update
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 export FLASK_APP=wsgi.py
 export FLASK_ENV=development
 # export APP_CONFIG_FILE=config.py
