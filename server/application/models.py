ecd096f8826ab0163801ccab78b389deb186d96f
bursalma <bursalma@gmail.com>
2020-05-03 17:43:42 -0700

    methodView

--- server/application/models.py
+++ server/application/models.py
@@ -1,28 +1,30 @@
 from . import db
+from datetime import datetime
 
 
-class User(db.Model):
-    """Data model for user accounts."""
+class Race(db.Model):
 
-    __tablename__ = 'user'
-    id = db.Column(db.Integer,
-                   primary_key=True)
-    username = db.Column(db.String(64),
-                         index=False,
-                         unique=True,
-                         nullable=False)
-    email = db.Column(db.String(80),
-                      index=True,
-                      unique=True,
-                      nullable=False)
-    created = db.Column(db.DateTime,
-                        index=False,
-                        unique=False,
-                        nullable=False)
-    admin = db.Column(db.Boolean,
-                      index=False,
-                      unique=False,
-                      nullable=False)
+    __tablename__ = 'race'
+    id = db.Column(db.Integer, primary_key=True)
+    race = db.Column(db.String(20), unique=True, nullable=False)
+    last_update = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def __init__(self, race):
+        self.race = race
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Race {}>'.format(self.race)
+
+
+class Nationality(db.Model):
+
+    __tablename__ = 'nationality'
+    id = db.Column(db.Integer, primary_key=True)
+    nationality = db.Column(db.String(45), unique=True, nullable=False)
+    last_update = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def __init__(self, nationality):
+        self.nationality = nationality
+
+    def __repr__(self):
+        return '<Nationality {}>'.format(self.nationality)
